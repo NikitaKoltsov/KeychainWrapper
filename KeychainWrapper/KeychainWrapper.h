6a8596fbaaa99df5c7b3cf2850213075b8d53e14
nikcolt <nikitoscolt@yandex.ru>
2019-09-21 15:19:47 +0300

    Initial Commit

---
+++ KeychainWrapper/KeychainWrapper.h
@@ -0,0 +1,19 @@
+//
+//  KeychainWrapper.h
+//  KeychainWrapper
+//
+//  Created by Nikita Koltsov on 9/21/19.
+//  Copyright © 2019 NIkita  Koltsov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeychainWrapper.
+FOUNDATION_EXPORT double KeychainWrapperVersionNumber;
+
+//! Project version string for KeychainWrapper.
+FOUNDATION_EXPORT const unsigned char KeychainWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainWrapper/PublicHeader.h>
+
+
